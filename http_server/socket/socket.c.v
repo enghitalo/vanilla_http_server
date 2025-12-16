0a3b45b628db046948cf2b1f2891494e1a35f456
Hitalo Souza <enghitalo@gmail.com>
2025-12-16 20:17:56 +0000

    os: make socket.c.v more compatible

--- http_server/socket/socket.c.v
+++ http_server/socket/socket.c.v
@@ -10,12 +10,22 @@ $if !windows {
 }
 
 fn C.socket(socket_family int, socket_type int, protocol int) int
-fn C.bind(sockfd int, addr &C.sockaddr_in, addrlen u32) int
+
+$if linux {
+	fn C.bind(sockfd int, addr &C.sockaddr_in, addrlen u32) int
+} $else {
+	fn C.bind(sockfd int, addr voidptr, addrlen u32) int // Use voidptr for generic sockaddr
+}
 fn C.setsockopt(__fd int, __level int, __optname int, __optval voidptr, __optlen u32) int
 fn C.listen(__fd int, __n int) int
-fn C.perror(s &u8)
+fn C.perror(s &char)
 fn C.close(fd int) int
-fn C.accept(sockfd int, address &C.sockaddr_in, addrlen &u32) int
+
+$if linux {
+	fn C.accept(sockfd int, address &C.sockaddr_in, addrlen &u32) int
+} $else {
+	fn C.accept(sockfd int, address voidptr, addrlen &u32) int // Use voidptr here too
+}
 fn C.htons(__hostshort u16) u16
 fn C.fcntl(fd int, cmd int, arg int) int
 fn C.connect(sockfd int, addr &C.sockaddr_in, addrlen u32) int
@@ -39,14 +49,15 @@ pub fn connect_to_server(port int) !int {
 		println('[client] Failed to create client socket')
 		return error('Failed to create client socket')
 	}
-	muts := C.sockaddr_in{
+	mut addr := C.sockaddr_in{
 		sin_family: u16(C.AF_INET)
 		sin_port:   C.htons(u16(port))
 		sin_addr:   C.in_addr{u32(0)} // 0.0.0.0
 		sin_zero:   [8]u8{}
 	}
 	println('[client] Connecting to server on port ${port} (0.0.0.0)...')
-	if C.connect(client_fd, &muts, sizeof(muts)) < 0 {
+	// Cast to voidptr for OS compatibility
+	if C.connect(client_fd, voidptr(&addr), sizeof(addr)) < 0 {
 		println('[client] Failed to connect to server')
 		C.close(client_fd)
 		return error('Failed to connect to server')
@@ -109,7 +120,8 @@ pub fn create_server_socket(port int) int {
 		sin_zero:   [8]u8{}
 	}
 
-	if C.bind(server_fd, &server_addr, sizeof(server_addr)) < 0 {
+	// Cast to voidptr to fix the type mismatch
+	if C.bind(server_fd, voidptr(&server_addr), sizeof(server_addr)) < 0 {
 		eprintln(@LOCATION)
 		C.perror(c'Bind failed')
 		close_socket(server_fd)
